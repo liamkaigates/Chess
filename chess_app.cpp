64a52f0212c9ea0ad104695c81e0a45ff5e560f4
liamkaigates.cs@gmail.com <liamgates19@icloud.com>
2024-01-12 15:09:38 -0500

    Pieces to bmp

--- chess_app.cpp
+++ chess_app.cpp
@@ -8,41 +8,102 @@
 
 using namespace std;
 
-int main(int argc, char *argv[])
+int init(SDL_Window **window, SDL_Surface **surface)
 {
-    SDL_Window *window = NULL;
-    SDL_Surface *surface = NULL;
     if (SDL_Init(SDL_INIT_VIDEO) < 0)
     {
         cout << "Error initializing SDL: " << SDL_GetError() << endl;
         SDL_Quit();
+        return -1;
+    }
+    *window = SDL_CreateWindow("Chess", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SQ_SIZE * DIMENSION, SQ_SIZE * DIMENSION, SDL_WINDOW_SHOWN);
+    if (*window == NULL)
+    {
+        cout << "Error initializing SDL Window: " << SDL_GetError() << endl;
         return 1;
     }
-    else
+    *surface = SDL_GetWindowSurface(*window);
+    return 0;
+}
+
+int loadBoard(SDL_Window **window, SDL_Surface **surface, SDL_Surface ***board)
+{
+    SDL_FillRect(*surface, nullptr, SDL_MapRGB((*surface)->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(*window);
+    char notation[3] = "A1";
+    for (int i = 0; i < DIMENSION; ++i)
     {
-        window = SDL_CreateWindow("Chess", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SQ_SIZE * 8, SQ_SIZE * 8, SDL_WINDOW_SHOWN);
-        if (window == NULL)
+        for (int j = 0; j < DIMENSION; ++j)
         {
-            printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-        }
-        else
-        {
-            surface = SDL_GetWindowSurface(window);
-            SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
-            SDL_UpdateWindowSurface(window);
-            SDL_Event e;
-            bool quit = false;
-            while (quit == false)
+            notation[0] = 'H' - i;
+            notation[1] = '1' + j;
+            cout << notation << endl;
+            if ((i + j) % 2 == 0)
             {
-                while (SDL_PollEvent(&e))
-                {
-                    if (e.type == SDL_QUIT)
-                        quit = true;
-                }
+            }
+            else
+            {
             }
         }
     }
+    return 0;
+}
+
+int loadPieces(SDL_Window **window, SDL_Surface **surface, SDL_Surface ***board)
+{
+    SDL_FillRect(*surface, nullptr, SDL_MapRGB((*surface)->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(*window);
+    char notation[3] = "A1";
+    for (int i = 0; i < DIMENSION; ++i)
+    {
+        for (int j = 0; j < DIMENSION; ++j)
+        {
+            notation[0] = 'H' - i;
+            notation[1] = '1' + j;
+            cout << notation << endl;
+            if ((i + j) % 2 == 0)
+            {
+            }
+            else
+            {
+            }
+        }
+    }
+    return 0;
+}
+
+int runGame()
+{
+    SDL_Event e;
+    bool quit = false;
+    while (!quit)
+    {
+        while (SDL_PollEvent(&e))
+        {
+            if (e.type == SDL_QUIT)
+                quit = true;
+        }
+    }
+    return 0;
+}
+
+int close(SDL_Window *window)
+{
     SDL_DestroyWindow(window);
     SDL_Quit();
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    SDL_Window *window = nullptr;
+    SDL_Surface *surface = nullptr;
+    SDL_Surface **board = nullptr;
+
+    init(&window, &surface);
+    loadBoard(&window, &surface, &board);
+    loadPieces(&window, &surface, &board);
+    runGame();
+    close(window);
     return 0;
 }
